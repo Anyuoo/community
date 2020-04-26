1494a9b34253f2b8d440581c947bea190ef25d8b
Anyuoo <2812431686@qq.com>
2020-04-26 15:52:56 +0800

    community

---
+++ src/main/java/com/anyu/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,41 @@
+package com.anyu.community.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@EnableAspectJAutoProxy
+@Component
+@Aspect
+public class ServiceLogAspect {
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    @Pointcut("execution(* com.anyu.community.service.*.*(..))")
+    public void servicePointcut() {
+    }
+
+    /**
+     * 用户[user]在[xxx]访问了[com.anyu.community.service.xxx]
+     */
+    @Before("servicePointcut()")
+    public void before(JoinPoint joinPoint) {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yy-DD-mm HH:mm:ss").format(new Date());
+
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s]在[%s]访问了[%s]", ip, now, target));
+    }
+}
